438b9568019c44d9f8a4bb0b5bf7ab0c5d4500ec
Igloongn <igloongn@gmail.com>
2022-06-17 09:29:36 +0100

    first commit

---
+++ ztest/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ztest project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ztest.settings')
+
+application = get_wsgi_application()
